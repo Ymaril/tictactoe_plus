c9671f0889ba545c9798f6a42ed4be3449253a68
Ymaril <a.timofeev@vmb-team.com>
2020-10-31 12:12:23 +0300

    Запрещает играть с самим собой

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,12 @@ class Game < ApplicationRecord
   belongs_to :x_player, class_name: 'User'
   belongs_to :o_player, class_name: 'User', required: false
 
+  validate do |game|
+    if game.x_player == game.o_player
+      errors.add(:o_player, :cant_be_the_same_as_x_player)
+    end
+  end
+
   state_machine :state, initial: :created do
     state all - [:created] do
       validates :o_player, presence: true
