f1abfab1dd1a21e18576b055978b13bcce120afe
Ymaril <a.timofeev@vmb-team.com>
2020-10-31 15:35:12 +0300

    Переводит на bootstrap

--- app/javascript/src/index/App/LoginPage.tsx
+++ app/javascript/src/index/App/LoginPage.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
-import { Card, Form, Input, Button, Error } from "./shared/AuthForms";
 import { useAuth } from "src/index/shared/auth";
 import api from "src/api";
+import {Alert, Button, Form} from "react-bootstrap";
 
 function LoginPage() {
   const { token, signIn } = useAuth();
@@ -22,27 +22,32 @@ function LoginPage() {
   }
 
   return (
-    <Card>
-      <Form>
-        <Input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="email"
-        />
-        <Input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="password"
-        />
-        <Button onClick={postLogin}>Sign In</Button>
-      </Form>
-      <Link to="/signup">Don't have an account?</Link>
+    <Form onSubmit={postLogin}>
       {errors.map((error, i) => (
-        <Error key={i}>{error}</Error>
+          <Alert key={i} variant="danger">{error}</Alert>
       ))}
-    </Card>
+
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Пароль</Form.Label>
+        <Form.Control type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Войти
+      </Button>
+    </Form>
   );
 }
 
--- app/javascript/src/index/App/SignupPage.tsx
+++ app/javascript/src/index/App/SignupPage.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
-import { Card, Form, Input, Button, Error } from "./shared/AuthForms";
 import { useAuth } from "src/index/shared/auth";
 import api from "src/api";
+import {Alert, Button, Form} from "react-bootstrap";
 
 function SignupPage() {
   const { token, signIn } = useAuth();
@@ -16,8 +16,8 @@ function SignupPage() {
       .post("/api/signup", {
         user: { email, password, password_confirmation: passwordConfirmation },
       })
-      .then((result) => signIn(result.headers.authorization, result.data))
-      .catch((e) => setErrors(e.response.data.errors));
+      .then(result => signIn(result.headers.authorization, result.data))
+      .catch(e => setErrors(e.response.data.errors));
   }
 
   if (token) {
@@ -25,32 +25,40 @@ function SignupPage() {
   }
 
   return (
-    <Card>
-      <Form>
-        <Input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="email"
-        />
-        <Input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="password"
-        />
-        <Input
-          type="password"
-          value={passwordConfirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-          placeholder="password again"
-        />
-        <Button onClick={postSignup}>Sign Up</Button>
+      <Form onSubmit={postSignup}>
+        {errors.map((error, i) => (
+            <Alert key={i} variant="danger">{error}</Alert>
+        ))}
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Пароль</Form.Label>
+          <Form.Control type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Подтверждение пароля</Form.Label>
+          <Form.Control type="password"
+                        value={passwordConfirmation}
+                        onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Регистрация
+        </Button>
       </Form>
-      {errors.map((error, i) => (
-        <Error key={i}>{error}</Error>
-      ))}
-    </Card>
   );
 }
 
