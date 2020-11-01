f87e4c33b104c0f93e52433abfaf93166534b888
Ymaril <a.timofeev@vmb-team.com>
2020-11-01 21:03:25 +0300

    Исправляет отображение клетки

--- app/javascript/src/index/App/GamePage/Board.tsx
+++ app/javascript/src/index/App/GamePage/Board.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import chunk_array from "array.chunk";
 import { Button, Row } from "react-bootstrap";
+import styled from "styled-components";
 
 interface Board {
   board: number[];
@@ -8,7 +9,12 @@ interface Board {
   locked?: boolean;
 }
 
-import "./Board.css";
+const Square = styled(Button)`
+  &:after {
+    content: "";
+    display: block;
+    padding-top: 100%;
+  }`;
 
 function Board(props: Board) {
   const board = chunk_array(props.board, 10);
@@ -34,7 +40,7 @@ function Board(props: Board) {
   return board.map((row, row_index) => (
     <Row key={row_index}>
       {row.map((square, square_index) => (
-        <Button
+        <Square
           className="square col m-1"
           key={square_index}
           disabled={props.locked || squares[square].locked}
@@ -42,7 +48,7 @@ function Board(props: Board) {
           onClick={(_) => props.onClick(row_index * 10 + square_index)}
         >
           {squares[square].text}
-        </Button>
+        </Square>
       ))}
     </Row>
   ));
