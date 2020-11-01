138152e0eee604e26fb96faeab060a8f38cd6460
Ymaril <a.timofeev@vmb-team.com>
2020-11-01 15:22:12 +0300

    Чинит bootstrap

--- app/javascript/src/index.tsx
+++ app/javascript/src/index.tsx
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index/App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
